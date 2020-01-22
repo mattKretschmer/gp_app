b47f8ac758129153cfabe98ff8d806edadf8697e
Matt <matt@Katies-iMac.local>
2020-01-21 23:33:12 -0500

    Adding numeric input

--- kedro-app/src/kedro_app/nodes/streamlit_app.py
+++ kedro-app/src/kedro_app/nodes/streamlit_app.py
@@ -19,7 +19,7 @@ if st.checkbox('Show noiseless function'):
     st.subheader('True Function')
     sns.lineplot(x=data, y=f_true(data))
     st.pyplot()
-
+x_new = st.number_input('Where should we measure?')
 # st.subheader('Number of pickups by hour:')
 # hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 # st.bar_chart(hist_values)
